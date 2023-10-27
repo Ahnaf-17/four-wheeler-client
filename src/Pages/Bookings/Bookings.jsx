0b628475f30d7bee0d6fdb21ba0230e205266c69
unknown <ahnaf.ahmed.42173@gmail.com>
2023-10-27 15:44:33 +0600

    delete operation complete

--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -7,6 +7,25 @@ const Bookings = () => {
     const [bookings,setBookings] = useState([])
     const url = `http://localhost:5000/bookings?email=${user?.email}`
 
+
+    const handleDelete = id => {
+        const proceed = confirm('Delete permanently?')
+        if(proceed){
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: "DELETE"
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if(data.deletedCount> 0){
+                    alert("deleted successfully")
+                    const remaining = bookings.filter(booking => booking._id !== id)
+                    setBookings(remaining)
+                }
+            })
+        }
+    }
+
     useEffect(()=>{
         fetch(url)
         .then(res => res.json())
@@ -14,26 +33,24 @@ const Bookings = () => {
     },[])
     return (
         <div>
-            {bookings.length}
             <div className="overflow-x-auto">
   <table className="table">
     {/* head */}
     <thead>
       <tr>
         <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
+          
         </th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-        <th></th>
+        <th>Image</th>
+        <th>Service</th>
+        <th>Date</th>
+        <th>Price</th>
+        <th>Status</th>
       </tr>
     </thead>
     <tbody>
         {
-            bookings.map(booking => <BookingRow key={booking._id} booking={booking}></BookingRow>)
+            bookings.map(booking => <BookingRow key={booking._id} booking={booking} handleDelete={handleDelete}></BookingRow>)
         }
 
     </tbody>
