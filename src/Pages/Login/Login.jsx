e1a0bda532b054459787dc89a0351eb957db922d
unknown <ahnaf.ahmed.42173@gmail.com>
2023-10-31 23:49:50 +0600

    token set in authprovider

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -17,16 +17,16 @@ const Login = () => {
         .then(result =>{
             const loggedInUser =result.user;
             console.log(loggedInUser)
-            const user = {email}
+            // const user = {email}
             
             // get access token 
-            axios.post('http://localhost:5000/jwt', user, {withCredentials:true})
-            .then(res => {
-                console.log(res.data)
-                if(res.data.success){
-                    navigate(location?.state?location?.state : '/')
-                }
-            })
+            // axios.post('http://localhost:5000/jwt', user, {withCredentials:true})
+            // .then(res => {
+            //     console.log(res.data)
+            //     if(res.data.success){
+            //         navigate(location?.state?location?.state : '/')
+            //     }
+            // })
 
         })
         .catch(error => console.error(error))
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,39 +1,49 @@
 /* eslint-disable react/prop-types */
 import { createContext, useEffect, useState } from "react";
-import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut} from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import app from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext();
 const auth = getAuth(app)
 
 
-const AuthProvider = ({children}) => {
-    const [user,setUser] = useState(null)
-    const [loading,setLoading] = useState(true)
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
 
-    const createUser = (email,password) =>{
+    const createUser = (email, password) => {
         setLoading(true)
-        return createUserWithEmailAndPassword(auth,email,password)
+        return createUserWithEmailAndPassword(auth, email, password)
     }
 
-    const SignIn =(email,password)=>{
+    const SignIn = (email, password) => {
         setLoading(true)
-        return signInWithEmailAndPassword(auth,email,password)
+        return signInWithEmailAndPassword(auth, email, password)
     }
 
-    const LogOut = () =>{
+    const LogOut = () => {
         return signOut(auth)
     }
-    useEffect(()=>{
-        const unSubscribe = onAuthStateChanged(auth,currentUser =>{
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
-            console.log('current user :',currentUser)
+            console.log('current user :', currentUser)
             setLoading(false)
+            // user exist give token
+            if (currentUser) {
+                const loggedUser = { email: currentUser.email }
+                axios.post('http://localhost:5000/jwt', loggedUser, { withCredentials: true })
+                    .then(res => {
+                        console.log('token : ', res.data)
+                    })
+            }
+
         });
-        return ()=> {
-            return unSubscribe(); 
+        return () => {
+            return unSubscribe();
         }
-    },[])
+    }, [])
 
 
     const authInfo = {
